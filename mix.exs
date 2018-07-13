c3cb072511e0c0db0edb481195b4a9b73bac358d
Bartosz Kalinowski <kelostrada@users.noreply.github.com>
2018-07-13 02:56:14 +0200

    allow lower version of poison as lots of dependencies still use ~> 2.0 (#25)

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule UeberauthTwitter.Mixfile do
     [
      {:httpoison, "~> 1.0"},
      {:oauther, "~> 1.1"},
-     {:poison, "~> 3.0"},
+     {:poison, "~> 2.0 or ~> 3.0"},
      {:ueberauth, "~> 0.4"},
 
      # dev/test dependencies
